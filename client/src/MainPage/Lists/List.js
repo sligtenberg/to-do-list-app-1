d30071c6ed2554590d96f30a06a9455625215ec1
stephen ligtenberg <bountyhunter24@gmail.com>
2023-08-24 17:22:14 -0400

    button styling

--- client/src/MainPage/Lists/List.js
+++ client/src/MainPage/Lists/List.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import Task from "./Task";
 
 function List({ userList }) {
   const tasks = userList.list.tasks.map(task => <Task key={task.id} task={task}/>)
 
+  const [expand, setExpand] = useState(false)
+
   return (
     <div className='list'>
+      <button className='float-left' onClick={() => setExpand(!expand)}>
+        {expand ? '-' : '+' }
+      </button>
+      <button className='float-right'>x</button>
       <h3>{userList.list.name}</h3>
       {tasks}
     </div>
